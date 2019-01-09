b8cd42f8ffdaea89df6c83aa2fa073360589ea78
alif33 <alifhasan332@gmail.com>
2019-01-09 13:52:07 +0600

    @Ready

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>crud system</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Our CRUD System</h1>
+
+
+
+    <div class="tbl">
+        <table>
+        <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Class</td>
+            <td>Result</td>
+            
+
+
+        </tr>
+        <tr>
+            <td>Name</td>
+            <td>Class</td>
+            <td>Result</td>
+            <td>Email</td>
+
+
+        </tr>
+        </table>
+    </div>
+</body>
+</html>
